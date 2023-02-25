b49f4e1ca7550d3af84a1da3913b9dfdd4c5d9b7
itsboutcoffe <120118166+itsboutcoffe@users.noreply.github.com>
2023-02-25 17:08:24 +0545

    Delete data.php

--- data.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost","root","","restro");
-if(!$conn){
-    die("Error: couldn't connect the file");
-}
-$stmt = "INSERT INT CRUD(holder, product, area, contact) VALUES (?,?,?,?)";
-$res = mysqli_query($conn,$stmt);
- 
